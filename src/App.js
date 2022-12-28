6425ed5b13da64c74bef2ab3eb7d8c60d50b2c15
Rehman Tyagi <81377973+RehmanTyagi@users.noreply.github.com>
2022-12-28 12:28:01 +0530

    jbfhjadsbf
    bdshbfhdsb

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 // import UserPlans from "./components/userPlans";
 import CertificatePlans from "./components/certPlans";
+import TopHeader from "./components/topHeader.js
 import React from "react";
 function App() {
-  return <CertificatePlans />;
+  return (<div>
+    <CertificatePlans />
+    <TopHeader/>
+    </div>);
 }
 
 export default App;
