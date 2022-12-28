53baa3eb48367efbf1ab51abce0d1e50336d17fd
Rehman Tyagi <81377973+RehmanTyagi@users.noreply.github.com>
2022-12-28 12:25:37 +0530

    dsfdsfbyu
    ysdyafvyasfv

--- src/components/appHeader.js
+++ src/components/appHeader.js
@@ -7,18 +7,23 @@ function topHeader() {
       </div>
       <ul className="list">
         <li className="list-item">
-          <a href="#" className="list-link">
-            Subscription
+          <a href="https://merchant.razorpay.com/policy/KxEikLW0irUrDj/privacy" className="list-link">
+            Privacy-Policy
+          </a>
+        </li>
+        <li className="list-item">
+          <a href="https://merchant.razorpay.com/policy/KxEikLW0irUrDj/terms" className="list-link">
+            Terms
           </a>
         </li>
         <li className="list-item">
           <a href="#" className="list-link">
-            About Us
+            Refund Policy
           </a>
         </li>
-        <li className="list-item">
-          <a href="#" className="list-link">
-            Domain Service
+         <li className="list-item">
+          <a href="https://merchant.razorpay.com/policy/KxEikLW0irUrDj/contact_us" className="list-link">
+            Contact Us
           </a>
         </li>
       </ul>
